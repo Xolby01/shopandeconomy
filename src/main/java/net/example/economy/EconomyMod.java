6e3fab0e01f84b681452ca40cbe1f8721f418d60
Xolby01 <nathwitk@gmail.com>
2025-08-14 19:51:56 +0200

    Update EconomyMod.java

--- src/main/java/net/example/economy/EconomyMod.java
+++ src/main/java/net/example/economy/EconomyMod.java
@@ -1,36 +1,30 @@
 package net.example.economy;
 
+import net.minecraft.server.MinecraftServer;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.event.server.ServerStoppingEvent;
-import net.neoforged.bus.api.SubscribeEvent;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.event.server.ServerStoppingEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraft.server.MinecraftServer;
-
-@Mod("shopandeconomy")
+@Mod(EconomyMod.MODID)
 public class EconomyMod {
-    public static MoneyManager moneyManager;
-    public static TradeManager tradeManager;
+    public static final String MODID = "shopandeconomy";
+
+    public static final MoneyManager moneyManager = new MoneyManager();
+    public static final TradeManager tradeManager = new TradeManager();
 
     public EconomyMod() {
-        moneyManager = new MoneyManager();
-        tradeManager = new TradeManager();
+        // Pas d’enregistrement particulier côté mod bus pour l’instant.
     }
 
     @SubscribeEvent
-    public void onServerStart(ServerStartingEvent event) {
+    public static void onServerStarting(ServerStartingEvent event) {
         MinecraftServer server = event.getServer();
         moneyManager.loadAll(server);
     }
 
     @SubscribeEvent
-    public void onServerStop(ServerStoppingEvent event) {
+    public static void onServerStopping(ServerStoppingEvent event) {
         MinecraftServer server = event.getServer();
         moneyManager.saveAll(server);
     }
